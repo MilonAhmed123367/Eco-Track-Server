00a2ee607b542c2359b95fd02bf2187d7e0eeed1
MilonAhmed123367 <activemilonrana123@gmail.com>
2025-11-10 00:17:16 +0600

    setUp complete

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+const MONGODB_URI = process.env.MONGODB_URI;
+//  || 'mongodb://127.0.0.1:27017/ecotrack'
+
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.send({ ok: true, message: 'EcoTrack API' });
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
